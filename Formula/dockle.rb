5bb1007d6fe45ea9f669ed30fd440a5a4301c4ef
goreleaserbot <goreleaser@carlosbecker.com>
2019-06-17 02:42:52 +0900

    Brew formula update for dockle version v0.1.11

--- Formula/dockle.rb
+++ Formula/dockle.rb
@@ -2,14 +2,14 @@
 class Dockle < Formula
   desc "Simple security auditing, helping build the Best Docker Images"
   homepage "https://github.com/goodwithtech/dockle"
-  version "0.1.10"
+  version "0.1.11"
 
   if OS.mac?
-    url "https://github.com/goodwithtech/dockle/releases/download/v0.1.10/dockle_0.1.10_macOS-64bit.tar.gz"
-    sha256 "ab90d0f1cf5fb64f8c823e4146cdc9a78a617ec0c3040231d8d448f4e083a643"
+    url "https://github.com/goodwithtech/dockle/releases/download/v0.1.11/dockle_0.1.11_macOS-64bit.tar.gz"
+    sha256 "4244d59a42d020ae0190a5f1ce49eb526e9b855c61a2b855d6ae1100de2dfcbe"
   elsif OS.linux?
-    url "https://github.com/goodwithtech/dockle/releases/download/v0.1.10/dockle_0.1.10_Linux-64bit.tar.gz"
-    sha256 "4c3aab311a21cc481e278fe07d3bea063c58f35f4f4c40b770888af39a680d6a"
+    url "https://github.com/goodwithtech/dockle/releases/download/v0.1.11/dockle_0.1.11_Linux-64bit.tar.gz"
+    sha256 "dbbb6f47b91b4ed6603c30b1354391f0ed0620ea209cf4e7a7880a116b0fb24b"
   end
 
   def install
